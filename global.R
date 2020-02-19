2930ac1e82d9d9b89cfbdeca07db916bf9d15d0a
sanmame <sanmame@gmail.com>
2020-02-19 14:00:21 +0100

    Update global.R
    it took too long to process the corpus on the server, so it now saves the text matrices as files

--- global.R
+++ global.R
@@ -4,7 +4,7 @@
 library(tm)
 library(wordcloud)
 library(memoise)
- 
+
 # The list of valid books
 books <<- list("Emma" = "emma",
                "Mansfield Park" = "mansfield",
@@ -15,21 +15,28 @@ books <<- list("Emma" = "emma",
 
 # Using "memoise" to automatically cache the results
 getTermMatrix <- memoise(function(book) {
-  text <- readLines(sprintf("./%s.txt.gz", book),
-                    encoding="UTF-8")
-
-  myCorpus = Corpus(VectorSource(text))
-  myCorpus = tm_map(myCorpus, content_transformer(tolower))
-  myCorpus = tm_map(myCorpus, removePunctuation)
-  myCorpus = tm_map(myCorpus, removeNumbers)
-  myCorpus = tm_map(myCorpus, removeWords,
-                    c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
-
-  myDTM = TermDocumentMatrix(myCorpus,
-                             control = list(minWordLength = 1))
-
-  m = as.matrix(myDTM)
-
-  sort(rowSums(m), decreasing = TRUE)
+    text <- readLines(sprintf("./%s.txt.gz", book),
+                      encoding="UTF-8")
+    
+    myCorpus = Corpus(VectorSource(text))
+    myCorpus = tm_map(myCorpus, content_transformer(tolower))
+    myCorpus = tm_map(myCorpus, removePunctuation)
+    myCorpus = tm_map(myCorpus, removeNumbers)
+    myCorpus = tm_map(myCorpus, removeWords,
+                      c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
+    
+    myDTM = TermDocumentMatrix(myCorpus,
+                               control = list(minWordLength = 1))
+    
+    m = as.matrix(myDTM)
+    
+    sort(rowSums(m), decreasing = TRUE)
 })
 
+getTextMatrixFiles <- function(){
+    for (book in books){
+        tm <- getTermMatrix(book)
+        filename <- paste0("tm_",book)
+        saveRDS(tm, file = "tm_sense.rds")
+    }
+}
